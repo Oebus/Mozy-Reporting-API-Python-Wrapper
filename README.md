939f57b108b324748a647e9ef268a188e2cb5c84
Oebus <thatsimonsguy@gmail.com>
2013-12-03 16:53:36 -0700

    Expanded output options for the report
    +option to return the raw python Requests object
    +option to retry when HTTP status code 202 is returned (when report is
    building)
    +API error handling for status codes outside of 202/200

--- README.md
+++ README.md
@@ -6,30 +6,24 @@ Provides a simplified Python interface for the Mozy Reporting API
 
 There are two main functions included in this library:
 
-1. getToken
+1. getToken()
+Returns a valid token object from Reporting API auth/exchange
 
-Parameters: API_key, auth_url (optional), xpartnerid (optional)
-	auth_url: default value http://services.mozy.com/auth/exchange
-		can be used to obtain the token from another provider, used for internal testing purposes
-	xpartnerid: default None
-		can be provided to return a key specific to a sub-partner of the parent account
-  
-Returns: a token object which can be used to fetch a report from the API
+kwarg "auth_url" can be used to change the request url (for internal testing)
+kwarg "xpartner_id" along with a specified sub-partner ID will return a token specific to the sub-partner
 
 Example:
 new_token = getToken("5Qhx6kLoGuej3y03RCa6Bly0HSQmp3R1GaBwex26BQlpTRi9mMDak7diBIV9vMKM")
 
-2. fetchReport
+2. fetchReport()
+Returns a report in python dictionary format (default)
 
-Parameters: token, report_type, scope, report_id
-	token: the token object returned from getToken()
-	report_type: either "resources" or "backup_status"
-	scope: can be "partner", "user_group", "user", or "machine"*
-	report_id: the id of the partner, user group, user, or machine
+arg "return_request_object" will return the raw python Requests object
+kwarg "wait_success" (dict object with keys "interval" and "timeout") will retry until timeout if the status code returned is 202 (report building)
 
-*resource reports can only be run for partners and user groups
-
-Returns: the requested report as a Python dictionary object
-
-Example:
+Examples:
 foo = fetchReport(token,"backup_status","partner","544098")
+foo = fetchReport(token,"backup_status","partner","544098","return_request_object")
+foo = fetchReport(token,"backup_status","partner","544098",wait_success={"interval":3,"timeout":15})
+
+where 'token' is a token object obtained from getToken
--- mozy_reporting_api.py
+++ mozy_reporting_api.py
@@ -2,15 +2,22 @@ import requests
 import time
 
 '''
-GetToken()
+getToken()
 Returns a valid token object from Reporting API auth/exchange
+
+kwarg "auth_url" can be used to change the request url (for internal testing)
+kwarg "xpartner_id" along with a specified sub-partner ID will return a token specific to the sub-partner
 
 Example:
 new_token = getToken("5Qhx6kLoGuej3y03RCa6Bly0HSQmp3R1GaBwex26BQlpTRi9mMDak7diBIV9vMKM")
 '''
-def getToken(API_key,auth_url="http://services.mozy.com/auth/exchange",xpartner_id=None):
+def getToken(API_key,**kwargs):
 
-	#sets up headers
+	#get kwargs
+	auth_url = kwargs.get("auth_url","http://services.mozy.com/auth/exchange")
+	xpartner_id = kwargs.get("xpartner_id", None)
+
+	#set up headers
 	request_header = {
 		"Accept":"application/vnd.mozy.bifrost+json;v=1",
 		"Api-Key": API_key
@@ -19,18 +26,18 @@ def getToken(API_key,auth_url="http://services.mozy.com/auth/exchange",xpartner_
 	if xpartner_id:
 		request_header["X-MozyPartner"] = str(xpartner_id)
 
-	#gets the token
+	#get the token
 	auth_request = requests.get(auth_url, headers=request_header)
 
-	#decodes json to python dict
+	#decode json to python dict
 	data = auth_request.json()
 
-	#calculates expiration timestamp
+	#calculate expiration timestamp
 	token_expiration = int(time.time()) + int(data["expires"]) - 5
 	
 	return Auth_Token(data["token_type"],data["token"],token_expiration,request_header,auth_url)
 
-class Auth_Token: #defines token object
+class Auth_Token: #defines the token object
 	def __init__(self, tok_type,tok_string,tok_exp,tok_head,tok_url):
 		self.type = tok_type
 		self.string = tok_string
@@ -54,15 +61,21 @@ class Auth_Token: #defines token object
 
 '''
 fetchReport()
-Returns a report in python dictionary format
+Returns a report in python dictionary format (default)
 
-Example:
+arg "return_request_object" will return the raw python Requests object
+kwarg "wait_success" (dict object with keys "interval" and "timeout") will retry until timeout
+	if the status code returned is 202 (report building)
+
+Examples:
 foo = fetchReport(token,"backup_status","partner","544098")
+foo = fetchReport(token,"backup_status","partner","544098","return_request_object")
+foo = fetchReport(token,"backup_status","partner","544098",wait_success={"interval":3,"timeout":15})
 
 where 'token' is a token object obtained from getToken
 '''
-def fetchReport(token,report_type,scope,report_id=None):
-
+def fetchReport(token,report_type,scope,report_id=None,*args,**kwargs):
+	
 	report_types_accepted = ["resources","backup_status"]
 	scopes_accepted = ["partner","user_group","user","machine"]
 
@@ -93,10 +106,28 @@ def fetchReport(token,report_type,scope,report_id=None):
 		"Authorization":"{} {}".format(request_token.type,request_token.string)
 	}
 
+	#get request
 	report_request = requests.get(request_url, headers=request_header)
 	
-	data = report_request.json()
-	return data
+	if "return_request_object" in args: #Option 1: return the raw request object
+		return report_request
+
+	elif "wait_success" in kwargs and report_request.status_code == 202: #Option 2: keep trying if status 202 returned
+		sleep_timer = kwargs.get("wait_success")["interval"] #set interval
+		time.sleep(sleep_timer)
+		if "timer" not in kwargs["wait_success"]: #create a timer var to keep track
+			kwargs["wait_success"]["timer"] = 0
+		kwargs["wait_success"]["timer"] += sleep_timer #update the timer
+		if kwargs.get("wait_success")["timeout"] <= kwargs.get("wait_success")["timer"]: #check the timer
+			raise Exception("Request timeout exceeded. Status code: 202") #timeout
+		else:
+			return fetchReport(token,report_type,scope,report_id,*args,**kwargs) #recurse
+
+	elif report_request.status_code != 202 and report_request.status_code != 200: #Successful API call but bad params
+		raise Exception(report_request.json()["message"]) #give error message if one is returned by API
+
+	else: #Option 3: if no errors, gimme the report in python dict format (default)
+		return report_request.json()
 
 def main(): #test script can go here
 	pass
